27c7dc153144db8fbef1dd4771d78fcac80a3dce
liangheteng1-ctrl <liangheteng1@gmail.com>
2026-05-30 20:33:08 +0800

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 var crypto = require("crypto");
 var MS = "f6d14b0ff265b8f3d5b2a30f138957a8";
-var LT = "geuJPKOIAK2vgcIhrNLXDmYXe97a/mUqUXjY4EGSnH1i/K5W2qi+VyH5R4aYgWNqzbEe7RtNuNQhedOqEZYvDmXv8D5qFfU517S9cV7lb9FC3zkSEwMIkfDtetPuxa46K2v6/WfzJN6SbPgZQOOjvgdB04t89/1O/w1cDnyilFU=";
+var LT = "Bbwmp9fxsh9+J/Gxv+n7OC2m78adJv8XfyUvTms3FMVD2usfIT9b27doUaZUJmNZzbEe7RtNuNQhedOqEZYvDmXv8D5qFfU517S9cV7lb9EVns37SwwXkGBwqtB6KOIvalaTTaDPNNhrNd41bGq1GwdB04t89/1O/w1cDnyilFU=";
 module.exports = async function(rq, rs) {
   if(rq.method!=="POST"){rs.status(405).end();return;}
   try{var raw=JSON.stringify(rq.body);var sig=rq.headers["x-line-signature"];if(!sig||crypto.createHmac("sha256",MS).update(raw).digest("base64")!==sig){rs.status(401).end();return;}}catch(e){rs.status(200).end();return;}
